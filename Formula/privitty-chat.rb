7794dd58d40900472569401103d71bc6ab2731b1
Milind Deore <milind.deore@privittytech.com>
2026-03-20 19:21:16 +0530

    Version v1.2.2 added

--- Formula/privitty-chat.rb
+++ Formula/privitty-chat.rb
@@ -1,8 +1,8 @@
 cask "privitty-chat" do
-  version "1.1.2"
-  sha256 "7b3089fa2c845812bc2417658084d4f5cd19362faf40db7bf273bea459a357d3"
+  version "1.2.2"
+  sha256 "41f4fef56f85c8ae14dd65a6262cdf0f43c93c25f0192a264f74e8917dcbd354"
 
-  url "https://github.com/Privitty/privitty-desktop/releases/download/v1.1.2/PrivittyChat-1.1.2-universal-signed.dmg"
+  url "https://github.com/Privitty/privitty-desktop/releases/download/v1.2.2/PrivittyChat-1.2.2-universal-signed.dmg"
   name "Privitty Chat"
   desc "Secure messaging platform with advanced encryption and granular access control. Enables end-to-end encrypted communication, secure file sharing, and policy-based data protection. Users control who can view, forward, download, or retain shared content, ensuring privacy and compliance."
   homepage "https://privittytech.com"
