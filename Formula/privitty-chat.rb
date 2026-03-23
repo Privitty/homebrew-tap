b29468304f837e5f7a8d15bd6577c40365c2afd2
Milind Deore <milind.deore@privittytech.com>
2026-03-23 13:31:30 +0530

    Upgraded dc-core to 2.15.0 and version 1.3.0

--- Formula/privitty-chat.rb
+++ Formula/privitty-chat.rb
@@ -1,8 +1,8 @@
 cask "privitty-chat" do
-  version "1.2.2"
-  sha256 "41f4fef56f85c8ae14dd65a6262cdf0f43c93c25f0192a264f74e8917dcbd354"
+  version "1.3.0"
+  sha256 "c3dd1b949a12d238ebeb0387264278eb17b67e5502d8800e855a864d9cf0d223"
 
-  url "https://github.com/Privitty/privitty-desktop/releases/download/v1.2.2/PrivittyChat-1.2.2-universal-signed.dmg"
+  url "https://github.com/Privitty/privitty-desktop/releases/download/v1.3.0/PrivittyChat-1.3.0-universal-signed.dmg"
   name "Privitty Chat"
   desc "Secure messaging platform with advanced encryption and granular access control. Enables end-to-end encrypted communication, secure file sharing, and policy-based data protection. Users control who can view, forward, download, or retain shared content, ensuring privacy and compliance."
   homepage "https://privittytech.com"
